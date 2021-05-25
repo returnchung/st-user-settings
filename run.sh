e327dfcd78b90e977dbd800d31cdc3da645005c5
returnchung <return1225@gmail.com>
2021-05-25 15:46:01 +0800

    feat: autocmt on 20210525154601

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-FOLDER="Sublime Text"
-path=$(find $HOME/Library/Application\ Support -type d -name $FOLDER 2>/dev/null);
-src=$subl3path/Packages/User;
+FOLDER="Sublime Text";
+path=$(find $HOME/Library/Application\ Support -type d -name "$FOLDER" 2>/dev/null);
+src=$path/Packages/User;
 dest=".";
 rsync -azvq "$src" "$dest" --exclude .git* --exclude .venv --exclude .DS_Store;
