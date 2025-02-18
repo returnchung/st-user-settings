2617c51ed0a8cfd990ce7e8443422ee5c33438b2
returnchung <return1225@gmail.com>
2025-02-18 11:00:06 +0800

    feat: autocmt on 2025-02-18T11:00:06

--- autocmt.sh
+++ autocmt.sh
@@ -2,11 +2,13 @@
 
 WORKDIR=$(dirname $(realpath "$0"));
 cd $WORKDIR;
-./run.sh;
+
+./sync.sh;
+
 any_chg=$(git diff | grep 'diff --git');
+DATE=`date +%Y-%m-%dT%H:%M:%S`;
 if [ ! -z "$any_chg" ]
 then
-    DATE=`date +%Y-%m-%dT%H:%M:%S`;
     git add . ;
     git commit -m "feat: autocmt on $DATE";
     git push origin;
